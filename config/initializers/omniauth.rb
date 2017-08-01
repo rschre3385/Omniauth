b090faecc8b653237f7e96a15cf4d57a15a32c06
Ryan Schreder <schredermail@yahoo.com>
2017-07-31 19:54:37 -0500

    first commit

---
+++ config/initializers/omniauth.rb
@@ -0,0 +1,6 @@
+OmniAuth.config.logger = Rails.logger
+
+Rails.application.config.middleware.use OmniAuth::Builder do
+  provider :facebook, ENV['FACEBOOK_APP_ID'], ENV['FACEBOOK_APP_SECRET']
+  provider :github, ENV['GITHUB_KEY'], ENV['GITHUB_SECRET'], scope: "user:email,user:follow"
+end
