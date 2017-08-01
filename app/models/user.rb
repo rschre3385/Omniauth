4c636743261d52786349cfa817f37a09e8715627
Ryan Schreder <schredermail@yahoo.com>
2017-07-31 20:14:53 -0500

    added github login to ominauth

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,11 +1,13 @@
 class User < ApplicationRecord
+
 	def self.create_with_omniauth(auth)
-	  create! do |user| 
-	  	user.provider = auth["provider"]
-	  	user.uid = auth["uid"]
-	  	user.name = auth["info"]["name"]
-	  end
-	end 
+    create! do |user|
+      user.provider = auth["provider"]
+      user.uid = auth["uid"]
+      user.name = auth["info"]["name"]
+      user.image = auth["info"]["image"]
+    end
+  end
 
 	def largeimage
 	  "http://graph.facebook.com/#{self.uid}/picture?type=large"
